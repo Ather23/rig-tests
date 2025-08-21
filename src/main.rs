39da57445d932cdde6bbe01649db69414f0d21ab
ather <rizviather6@gmail.com>
2025-08-21 07:25:58 -0400

    open ai tools

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use rig::completion::Prompt;
 use rig::prelude::*;
 use rig::streaming::stream_to_stdout;
 use rig::{ completion::ToolDefinition, providers, streaming::StreamingPrompt, tool::Tool };
@@ -15,10 +16,16 @@ async fn main() -> Result<(), anyhow::Error> {
     let openai_client = providers::openai::Client::from_env();
 
     let rest_api_agent = openai_client
-        .agent(providers::openai::GPT_4O_MINI)
-        .preamble("You are an agent that can call RESTful APIs using reqwest.")
+        .agent(providers::openai::GPT_4)
+        .preamble(
+            "You are helpful assistant that has access to tools. 
+            You are going to help the user answer any questions using the tools provided.
+            You can also search the web for help."
+        )
         .max_tokens(1024)
         .tool(RestApiTool)
+        .tool(WebSearch)
+        .tool(ShellTool)
         .build();
 
     // Create agent with a single context prompt and two tools
@@ -36,13 +43,32 @@ async fn main() -> Result<(), anyhow::Error> {
     // You also have access to the web if you want to look up documentation.
     // Make sure you respond in a nice and friendly way.
 
+    /**
+     * 
+     *             "You are an assistant here to help the user select which tool is most appropriate to perform arithmetic operations.
+            Follow these instructions closely.
+            1. Consider the user's request carefully and identify the core elements of the request.
+            2. Select which tool among those made available to you is appropriate given the context.
+            3. This is very important: never perform the operation yourself.
+            "
+     * 
+     */
+
     // You are a web search agent that can return results based on users query. You also have shell access if you need it.
+    // "Call the REST API at https://jsonplaceholder.typicode.com/todos/1 and show the result."
 
-    let mut stream = rest_api_agent.stream_prompt(
-        "Call the REST API at https://jsonplaceholder.typicode.com/todos/1 and show the result."
-    ).await?;
+    let result = rest_api_agent
+        .prompt("Clone the langchain repo in D:\\test-rig-agent")
+        .multi_turn(20).await?;
 
-    stream_to_stdout(&rest_api_agent, &mut stream).await?;
+    println!("\n\nAgent Response: {result}");
+
+    // let mut stream = rest_api_agent.stream_prompt(
+    //     "Your tasks is to clone langchain repo in D:\\test-rig folder.
+    //     Create the folder if not there. Try to fix the command if its incorrect"
+    // ).await?;
+
+    // stream_to_stdout(&rest_api_agent, &mut stream).await?;
 
     // if let Some(response) = stream.response {
     //     println!("Usage: {:?} tokens", response.);
